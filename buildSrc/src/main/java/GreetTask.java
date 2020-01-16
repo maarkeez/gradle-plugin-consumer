1370c572a4a6b444db55d684bb489eae794a3247
maarkeez <d.marquez.delgado.91@gmail.com>
2020-01-16 23:54:38 +0100

    Initial commit

---
+++ buildSrc/src/main/java/GreetTask.java
@@ -0,0 +1,10 @@
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class GreetTask extends DefaultTask {
+	
+	@TaskAction
+	public void greetAction() {
+		System.out.println("Hello gradle plugin");
+	}
+}
